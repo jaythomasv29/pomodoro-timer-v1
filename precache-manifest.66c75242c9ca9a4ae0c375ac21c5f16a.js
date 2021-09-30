9fbda6fcca998d3e399b9be4e3f10e1127ed5778
James Vongampai <jaythomasv29@gmail.com>
2021-09-30 10:54:12 -0700

    Updates

---
+++ precache-manifest.66c75242c9ca9a4ae0c375ac21c5f16a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba90f8aee545ad7100efe83a407678d9",
+    "url": "/pomodoro-timer-v1/index.html"
+  },
+  {
+    "revision": "0effb0ee46cb46d4fa97",
+    "url": "/pomodoro-timer-v1/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro-timer-v1/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer-v1/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0effb0ee46cb46d4fa97",
+    "url": "/pomodoro-timer-v1/static/js/main.ab114da3.chunk.js"
+  },
+  {
+    "revision": "a8b837fbe16a352ac551",
+    "url": "/pomodoro-timer-v1/static/js/runtime-main.d6f9519a.js"
+  }
+]);
