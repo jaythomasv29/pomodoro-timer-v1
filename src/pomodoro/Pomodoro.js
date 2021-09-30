04639adab4020d261de488514dc47508af024982
James Vongampai <34979065+jaythomasv29@users.noreply.github.com>
2021-09-30 10:43:40 -0700

    Add files via upload
    add project files with separate components for PlayButton, BreakButton, FocusButton and TimerDisplay

---
+++ src/pomodoro/Pomodoro.js
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import useInterval from "../utils/useInterval";
+import { secondsToDuration } from "../utils/duration";
+import { minutesToDuration } from "../utils/duration";
+import FocusButton from "./FocusButton";
+import BreakButton from "./BreakButton";
+import PlayPauseButton from "./PlayPauseButton";
+import TimerDisplay from "./TimerDisplay";
+// These functions are defined outside of the component to insure they do not have access to state
+// and are, therefore more likely to be pure.
+
+/**
+ * Update the session state with new state after each tick of the interval.
+ * @param prevState
+ *  the previous session state
+ * @returns
+ *  new session state with timing information updated.
+ */
+function nextTick(prevState) {
+  const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
+  return {
+    ...prevState,
+    timeRemaining,
+  };
+}
+
+/**
+ * Higher order function that returns a function to update the session state with the next session type upon timeout.
+ * @param focusDuration
+ *    the current focus duration
+ * @param breakDuration
+ *    the current break duration
+ * @returns
+ *  function to update the session state.
+ */
+function nextSession(focusDuration, breakDuration) {
+  /**
+   * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
+   */
+  return (currentSession) => {
+    if (currentSession.label === "Focusing") {
+      return {
+        label: "On Break",
+        timeRemaining: breakDuration * 60,
+      };
+    }
+    return {
+      label: "Focusing",
+      timeRemaining: focusDuration * 60,
+    };
+  };
+}
+
+function Pomodoro() {
+  // Timer starts out paused
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  
+  const [progressBarCalc, setProgressBarCalc] = useState(0);
+
+  // The current session - null where there is no session running
+  const [session, setSession] = useState(null);
+
+  // ToDo: Allow the user to adjust the focus and break duration.
+  const [focusDuration, setFocusDuration] = useState(25);
+
+  const [breakDuration, setbreakDuration] = useState(5);
+
+  /**
+   * Custom hook that invokes the callback function every second
+   *
+   * NOTE: You will not need to make changes to the callback function
+   */
+  useInterval(() => {
+      if (session.timeRemaining === 0) {
+        new Audio("https://bigsoundbank.com/UPLOAD/mp3/2386.mp3").play();
+        setSession(nextSession(focusDuration, breakDuration));
+      }
+      setSession(nextTick);
+    
+    if(session.label === "Focusing"){
+      //set
+      setProgressBarCalc(((focusDuration*60) - session.timeRemaining) / (focusDuration*60) * 100);
+      //console.log(progressBarCalc);
+    } else{
+      setProgressBarCalc(((breakDuration*60) - session.timeRemaining) / (breakDuration*60) * 100);
+      //console.log(progressBarCalc);
+    }
+  },
+    isTimerRunning ? 1000 : null
+  );
+
+  /**
+   * Called whenever the play/pause button is clicked.
+   */
+  function playPause() {
+    setIsTimerRunning((prevState) => {
+
+      const nextState = !prevState;
+      if (nextState) {
+        setSession((prevStateSession) => {
+          // If the timer is starting and the previous session is null,
+          // start a focusing session.
+          if (prevStateSession === null) {
+            return {
+              label: "Focusing",
+              timeRemaining: focusDuration * 60,
+            };
+          }
+          return prevStateSession;
+        });
+      }
+      return nextState;
+    });
+  }
+
+  const handleIncreaseFocus = () => {
+    // pure function that has a task of increasing focus duration Math.min acts as a limiter if focusState becomes >60
+      setFocusDuration((lastFocusDuration) => Math.min(60, lastFocusDuration + 5));
+  };
+
+  const handleDecreaseFocus = () => {
+    // pure function that has a task of decrease focus duration Math.max acts as a limiter if focusState becomes < 5
+    setFocusDuration((lastFocusDuration) => Math.max(5, lastFocusDuration - 5));
+  };
+
+  const handleDecreaseBreak = () => {
+    setbreakDuration((lastbreakDuration) => Math.max(1, lastbreakDuration - 1));
+  };
+
+  const handleIncreaseBreak = () => {
+    setbreakDuration((lastbreakDuration) => Math.min(15, lastbreakDuration + 1));
+  };
+
+  const handleStopButton = () => {
+    setSession(null);
+    setIsTimerRunning(false);
+  };
+
+   
+  return (
+    <div className="pomodoro">
+      <div className="row">
+        <div className="col">
+          <FocusButton
+          focusDuration={focusDuration}
+          minutesToDuration={minutesToDuration}
+          isTimerRunning={isTimerRunning}
+          handleDecreaseFocus={handleDecreaseFocus}
+          handleIncreaseFocus={handleIncreaseFocus}
+          />
+        </div>
+        <div className="col">
+          <div className="float-right">
+            <BreakButton
+            breakDuration={breakDuration}
+            minutesToDuration={minutesToDuration}
+            isTimerRunning={isTimerRunning}
+            handleDecreaseBreak={handleDecreaseBreak}
+            handleIncreaseBreak={handleIncreaseBreak}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <PlayPauseButton
+            playPause={playPause}
+            isTimerRunning={isTimerRunning}
+            handleStopButton={handleStopButton}
+          />
+        </div>
+      </div>
+      <TimerDisplay
+      minutesToDuration={minutesToDuration}
+      secondsToDuration={secondsToDuration}
+      focusDuration={focusDuration}
+      breakDuration={breakDuration}
+      session={session}
+      isTimerRunning={isTimerRunning}
+      progressBarCalc={progressBarCalc}
+      />
+    </div>
+  );
+}
+
+export default Pomodoro;
